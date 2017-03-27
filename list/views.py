263cfa9df25cb3c6bec8dcd94c5fd11533291ecd
Juan Rodriguez <juank.rh27@gmail.com>
2017-03-27 12:23:25 -0400

    - Added static files - Separated templates and corresponding CSS files

--- list/views.py
+++ list/views.py
@@ -4,14 +4,14 @@ from django.template import loader
 
 # Create your views here.
 def index(request):
-	template = loader.get_template('base.html')
-	context = {site : 'site'}
+	template = loader.get_template('index.html')
+	context = {'site' : 'index'}
 	return HttpResponse(template.render(context, request));
 
 def list(request):
-	context = {site: 'list'}
-	render(request,'list.html',context);
+	context = {'site': 'list'}
+	return render(request,'list.html',context);
 
 def admin(request):
-	context = {site: 'admin'}
-	render(request,'admin.html',context);
+	context = {'site': 'admin'}
+	return render(request,'admin.html',context);
--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^list/', include('list.urls')),
+	url(r'^listApp/', include('list.urls')),
     url(r'^admin/', admin.site.urls),
 ]
