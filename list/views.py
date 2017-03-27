aa3c9206afd034df5a2d6a011e5d8a841faa0652
Juan Rodriguez <juank.rh27@gmail.com>
2017-03-27 07:29:02 -0400

    Changed names to match functionality

--- list/views.py
+++ list/views.py
@@ -5,10 +5,13 @@ from django.template import loader
 # Create your views here.
 def index(request):
 	template = loader.get_template('base.html')
+	context = {site : 'site'}
 	return HttpResponse(template.render(context, request));
 
 def list(request):
-	return HttpResponse(template.render(context, request))
+	context = {site: 'list'}
+	render(request,'list.html',context);
 
 def admin(request):
-	return HttpResponse();
+	context = {site: 'admin'}
+	render(request,'admin.html',context);
