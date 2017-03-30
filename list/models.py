82318ef99f19df6195f3d95f26f14ff9127a2925
Juan Rodriguez <juank.rh27@gmail.com>
2017-03-30 14:44:35 -0400

    Updated required fields in Models

--- list/models.py
+++ list/models.py
@@ -13,13 +13,12 @@ class Brother(models.Model):
 	brother_id = models.CharField(max_length = 30, primary_key=True)
 	first_n = models.CharField(max_length = 20)
 	last_n = models.CharField(max_length = 20)
-	status = models.CharField(max_length = 1, choices= STATUS, blank = True)
+	status = models.CharField(max_length = 1, choices= STATUS, required = False)
 	def __str__ (self):
 		output = self.first_n + " " + self.last_n
 		return output
 
 	#status: Either Active or Pledge
-
 class Guest(models.Model):
 	GENDER = (
 		('M', 'Male'),
@@ -29,7 +28,7 @@ class Guest(models.Model):
 	first_n = models.CharField(max_length = 30)
 	last_n = models.CharField(max_length= 30)
 	gender = models.CharField(max_length=1, choices=GENDER)
-	blacklisted = models.BooleanField(default=False) 
+	blacklisted = models.BooleanField(default=False, required = False)
 	host = models.ManyToManyField(Brother)
 	def __str__ (self):
 		return self.first_n + " " + self.last_n
@@ -39,5 +38,3 @@ class Guest(models.Model):
 # class Invite(models.Model):
 # 	host = models.ForeignKey(Brother, on_delete=models.CASCADE)
 # 	guest = models.ForeignKey(Guest, on_delete = models.CASCADE)
-
-
